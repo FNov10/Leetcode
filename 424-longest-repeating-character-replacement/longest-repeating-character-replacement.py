09f5bb70332df5d8a342483e8bb3c6f41cc9f731
Fahad Naveed <96806925+FNov10@users.noreply.github.com>
2026-07-04 15:37:43 -0400

    Time: 229 ms (9.24%) | Memory: 19.6 MB (49.75%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,7 +3,6 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         maxLen = 0
         L = 0
-        R = 0
         windowHash = {letter: 0 for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
         currLen = 0
         for R, value in enumerate(s):
