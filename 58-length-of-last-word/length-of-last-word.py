975e9ff350e06a8790cd921df56d1d22b7aa1041
Fahad Naveed <96806925+FNov10@users.noreply.github.com>
2026-07-27 19:07:52 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.92%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,3 +1,14 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+        R = len(s)-1
+        finalIndex = 0
+        while R>=0 and not re.match( r'[A-Za-z]', s[R]):
+            R-=1
+
+        # Now, we reached a non whitespace. This is our last word
+        counter = 0
+
+        while R>=0 and re.match(r'[A-Za-z]',s[R]):
+            counter+=1
+            R-=1
+        return counter
