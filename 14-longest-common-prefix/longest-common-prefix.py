5b46264ea7274af4ccc3e3980c720449de4ceac9
Fahad Naveed <96806925+FNov10@users.noreply.github.com>
2026-07-27 19:20:09 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,11 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        result = ""
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return result
-            result+=strs[0][i]
-        return result
-        
-        
+        # We choose an arbritary item in the list as our loop length
+        # doesn't matter which as each item will contain the minimum prefix
+        final = ""
+        completed = False
+        for index in range(len(strs[0])):
+            if completed:
+                break
+            for item in strs:
+                if not item:
+                    completed = True
+                elif index>=len(item):
+                    completed=True
+                elif item[index] == strs[0][index]:
+                    pass
+                else:
+                    completed = True
+            if not completed:
+                final+=item[index]
+
+        return final
