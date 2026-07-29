21c4bb1086e595e668259f0f1b95689641f1aaa2
Fahad Naveed <96806925+FNov10@users.noreply.github.com>
2026-07-28 21:02:41 -0400

    Time: 59 ms (9.78%) | Memory: 32.3 MB (87.10%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -2,16 +2,16 @@ from collections import Counter
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         window = {nums[0]}
-        L, R = 0, 1
-        while R<len(nums) and L<R:
+        L = 0
+        for R in range(1, len(nums)):
             while abs(R-L)>k:
                 window.remove(nums[L])
                 L+=1
             if nums[R] in window:
                 return True
             window.add(nums[R])
-            R+=1
         return False
+
 
             
 
